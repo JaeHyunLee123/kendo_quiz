e10e2139f5623b60a28893447b7e4217a8a9a62d
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2024-09-22 16:09:36 +0900

    feat: add register to problem input

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -2,42 +2,69 @@
 
 import ProblemInput from "@/components/ProblemInput";
 import ProblemTitle from "@/components/ProblemTitle";
-import { Input } from "@/components/ui/Input";
-import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/Button";
 import { useForm } from "react-hook-form";
 
-interface QuizForm {
+interface QuizAnswer {
+  oneA: number;
+  oneB: number;
+  oneC: number;
+}
+
+export interface QuizForm {
+  quizAnswer: QuizAnswer;
   name: string;
   id: number;
 }
 
 const Quiz = ({}) => {
-  const {} = useForm<QuizForm>();
+  const { register, handleSubmit } = useForm<QuizForm>();
+
+  const onSubmit = (form: QuizForm) => {
+    console.log(form);
+  };
 
   return (
     <main>
-      <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <ProblemTitle
           problemNumber={1}
           problemText="다음 빈칸에 알맞은 숫자를 기입하시오.(각 3점, 총 9점)"
         />
         <p className="indent-1">
-          시합 시작 시 <span className="px-2 border-2 border-black">a</span>
+          시합 시작 시 <span className="px-2 border-2 border-black">A</span>
           이후 허리칼을 하고
-          <span className="px-2 border-2 border-black">b</span>보 걸어나오며
+          <span className="px-2 border-2 border-black">B</span>보 걸어나오며
           뽑아칼을 한다.
         </p>
         <p className="indent-1">
           시합 종료 후 꽂아칼을 하고
-          <span className="px-2 border-2 border-black">c</span>보 뒷걸음 후
+          <span className="px-2 border-2 border-black">C</span>보 뒷걸음 후
           허리칼을 풀고 인사 후 시합장을 나간다.
         </p>
         <div className="flex items-center space-x-3">
-          <ProblemInput id="1-a" inputWidth={12} labelText="A" />
-          <ProblemInput id="1-b" inputWidth={12} labelText="B" />
-          <ProblemInput id="1-c" inputWidth={12} labelText="C" />
+          <ProblemInput
+            id="1-a"
+            inputWidth={12}
+            labelText="A"
+            {...register("quizAnswer.oneA", { valueAsNumber: true })}
+          />
+          <ProblemInput
+            id="1-b"
+            inputWidth={12}
+            labelText="B"
+            {...register("quizAnswer.oneB", { valueAsNumber: true })}
+          />
+          <ProblemInput
+            id="1-c"
+            inputWidth={12}
+            labelText="C"
+            {...register("quizAnswer.oneC", { valueAsNumber: true })}
+          />
         </div>
-      </div>
+
+        <Button>Submit</Button>
+      </form>
     </main>
   );
 };
