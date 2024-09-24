3da10cf0168480944cf1feb7b3872d32c9865254
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2024-09-24 13:27:56 +0900

    fix: fix ranking page to SSR

--- src/app/ranking/page.tsx
+++ src/app/ranking/page.tsx
@@ -1,13 +1,10 @@
 // ranking/page.tsx
 import { db } from "@/lib/db";
+import { User } from "@prisma/client";
 
-const Ranking = async () => {
+export const getServerSideProps = async () => {
   try {
     const ranking = await db.user.findMany({});
-
-    if (!ranking || ranking.length === 0) {
-      return <div>아직 답을 제출한 사람이 없습니다.</div>;
-    }
 
     ranking.sort((a, b) => {
       if (a.score > b.score) {
@@ -23,33 +20,54 @@ const Ranking = async () => {
       }
     });
 
-    return (
-      <main>
-        <span className="text-sm">
-          랭킹은 점수 순, 점수가 같으면 제출 시간 순으로 매깁니다.
-        </span>
-        {ranking.map((student, i) => (
-          <div key={i} className="space-x-2">
-            <span>{`${i + 1}등`}</span>
-            <span>{student.name}</span>
-            <span>{`${student.score}점`}</span>
-            <span>{`${
-              student.submitTime
-                ? student.submitTime.toISOString().slice(5, 10)
-                : "N/A"
-            } ${
-              student.submitTime
-                ? student.submitTime.toISOString().slice(11, 19)
-                : ""
-            } 제출`}</span>
-          </div>
-        ))}
-      </main>
-    );
+    return {
+      props: {
+        ranking,
+      },
+    };
   } catch (error) {
     console.error("Failed to load ranking:", error);
-    return <div>데이터를 불러오는 중 오류가 발생했습니다.</div>;
+    return {
+      props: {
+        ranking: [],
+        error: "데이터를 불러오는 중 오류가 발생했습니다.",
+      },
+    };
   }
+};
+
+const Ranking = ({ ranking, error }: { ranking: User[]; error: string }) => {
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  if (!ranking || ranking.length === 0) {
+    return <div>아직 답을 제출한 사람이 없습니다.</div>;
+  }
+
+  return (
+    <main>
+      <span className="text-sm">
+        랭킹은 점수 순, 점수가 같으면 제출 시간 순으로 매깁니다.
+      </span>
+      {ranking.map((student, i) => (
+        <div key={i} className="space-x-2">
+          <span>{`${i + 1}등`}</span>
+          <span>{student.name}</span>
+          <span>{`${student.score}점`}</span>
+          <span>{`${
+            student.submitTime
+              ? student.submitTime.toISOString().slice(5, 10)
+              : "N/A"
+          } ${
+            student.submitTime
+              ? student.submitTime.toISOString().slice(11, 19)
+              : ""
+          } 제출`}</span>
+        </div>
+      ))}
+    </main>
+  );
 };
 
 export default Ranking;
